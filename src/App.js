9bc9b8845e443a6ae98bd8b33a837ed466075025
LAPTOP-T21LK98P\KHS <https://github.com/Hosae-Coding>
2021-11-21 23:42:07 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import comic1 from './img/comic01.jpg';
+import comic2 from './img/comic02.jpg';
+import comic3 from './img/comic03.jpg';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const [move, SetMove] = useState(0);
+
+   const cards = [
+      { id: 1, image: comic1 },
+      { id: 2, image: comic2 },
+      { id: 3, image: comic3 },
+   ];
+
+   const mod = (n, m) => {
+      let result = n % m;
+
+      return result >= 0 ? result : result + m;
+   };
+
+   useEffect(() => {
+      setTimeout(() => {
+         SetMove((move + 1) % cards.length);
+      }, 3000);
+   }, [move]);
+   return (
+      <div className="App">
+         <div className="wrap">
+            {cards.map((item, i) => {
+               const indexLeft = mod(move - 1, cards.length);
+               const indexRight = mod(move + 1, cards.length);
+
+               let className = '';
+
+               if (i === move) {
+                  className = 'card card__active';
+               } else if (i === indexRight) {
+                  className = 'card card__right';
+               } else if (i === indexLeft) {
+                  className = 'card card__left';
+               } else {
+                  className = 'card';
+               }
+               return (
+                  <img
+                     key={item.id}
+                     src={item.image}
+                     alt="card"
+                     className={className}
+                  />
+               );
+            })}
+         </div>
+      </div>
+   );
 }
 
 export default App;
